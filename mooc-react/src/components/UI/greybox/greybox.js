5a5bb65efa21bafb433182b2f3a167317b5e9011
sabil62 <shresthasabil62@gmail.com>
2021-06-07 18:07:52 +0545

    animated side grey box

--- mooc-react/src/components/UI/greybox/greybox.js
+++ mooc-react/src/components/UI/greybox/greybox.js
@@ -1,29 +1,75 @@
-import React from "react";
+import React, { Component } from "react";
 import "./greybox.css";
 import { DownSquareOutlined, UpSquareOutlined } from "@ant-design/icons";
 
-const greybox = (props) => {
-  return (
-    <div className="greybox">
-      <div className="greybox-up-title">
-        <div className="greybox-left-title">
-          <img
-            src={props.logo}
-            style={{ height: "36px", marginRight: "1rem" }}
-          />
-          {props.title}
+class Greybox extends Component {
+  state = {
+    downContent: true,
+  };
+  render() {
+    return (
+      <div className="greybox">
+        <div className="greybox-up-title">
+          <div className="greybox-left-title">
+            <img
+              src={this.props.logo}
+              style={{ height: "36px", marginRight: "1rem" }}
+            />
+            {this.props.title}
+          </div>
+          <div className="greybox-right-title">
+            {!this.state.downContent ? (
+              <DownSquareOutlined onClick={() => this.onDownContentDisplay()} />
+            ) : (
+              <UpSquareOutlined onClick={() => this.onDownContentNoDisplay()} />
+            )}
+          </div>
         </div>
-        <div className="greybox-right-title">
-          <DownSquareOutlined />
-          <UpSquareOutlined />
+        <div style={{ margin: "10px 16px" }}>
+          <div className="greybox-line"></div>
+          <div className="greybox-line dark"></div>
         </div>
+        {this.state.downContent ? (
+          <div className="show-content">{this.props.children}</div>
+        ) : null}
       </div>
-      <div style={{ margin: "10px 16px" }}>
-        <div className="greybox-line"></div>
-        <div className="greybox-line dark"></div>
-      </div>
-    </div>
-  );
-};
+    );
+  }
+  onDownContentNoDisplay = () => {
+    // this.setState((prevState) => {
+    //   downContent: !prevState.downContent;
+    // });
+    this.setState({ downContent: false });
+  };
+  onDownContentDisplay = () => {
+    this.setState({ downContent: true });
+  };
+}
 
-export default greybox;
+export default Greybox;
+
+// const greybox = (props) => {
+//   return (
+//     <div className="greybox">
+//       <div className="greybox-up-title">
+//         <div className="greybox-left-title">
+//           <img
+//             src={props.logo}
+//             style={{ height: "36px", marginRight: "1rem" }}
+//           />
+//           {props.title}
+//         </div>
+//         <div className="greybox-right-title">
+//           <DownSquareOutlined />
+//           <UpSquareOutlined />
+//         </div>
+//       </div>
+//       <div style={{ margin: "10px 16px" }}>
+//         <div className="greybox-line"></div>
+//         <div className="greybox-line dark"></div>
+//       </div>
+//     </div>
+//   );
+// };
+
+// export default greybox;
--- mooc-react/src/components/main-content/main-content.js
+++ mooc-react/src/components/main-content/main-content.js
@@ -3,17 +3,64 @@ import "./main-content.css";
 import Greybox from "../UI/greybox/greybox";
 import SettingsLogo from "../../assets/HCI/batches side/settings.png";
 import LevelUpLogo from "../../assets/HCI/batches side/Level up.png";
+import {
+  UserOutlined,
+  CaretDownOutlined,
+  FileMarkdownFilled,
+  RocketFilled,
+  CaretRightFilled,
+} from "@ant-design/icons";
 
 class MainContent extends Component {
-  // state = {};
+  state = {
+    thingsTodo: [
+      { topic: "Internal I", daysRemaining: 7 },
+
+      { topic: "Quiz | Week 7", daysRemaining: 7 },
+      { topic: "Quiz | Week 3", daysRemaining: 7 },
+      { topic: "VARK", daysRemaining: 7 },
+      { topic: "ILS", daysRemaining: 7 },
+      { topic: "Discussion week-4", daysRemaining: 7 },
+    ],
+  };
   render() {
     return (
       <React.Fragment>
         <div className="row">
           <div className="col-lg-3 col-sm-4 col-12">
-            <Greybox logo={SettingsLogo} title={"Administration"}></Greybox>
-            <div style={{ height: "1.4rem" }}></div>
-            <Greybox logo={LevelUpLogo} title={"Things to do "}></Greybox>
+            <Greybox logo={SettingsLogo} title={"Administration"}>
+              <div style={{ lineHeight: "1.4" }}>
+                <CaretDownOutlined /> Course Administration
+                <div style={{ marginLeft: "18px" }}>
+                  <div>
+                    <UserOutlined style={{ marginRight: "3px" }} />
+                    Unenrol me from COMP 341
+                  </div>
+                  <div>
+                    <FileMarkdownFilled style={{ marginRight: "3px" }} />
+                    Grades
+                  </div>
+                  <div>
+                    <RocketFilled style={{ marginRight: "3px" }} />
+                    Competiencies
+                  </div>
+                </div>
+              </div>
+            </Greybox>
+            <Greybox logo={LevelUpLogo} title={"Things to do"}>
+              {this.state.thingsTodo.map((c, index) => (
+                <div key={index} className="things-to-do">
+                  <div>
+                    {" "}
+                    <CaretRightFilled />
+                    {c.topic}
+                  </div>
+                  <div style={{ opacity: "0.6" }}>
+                    {c.daysRemaining} days rem
+                  </div>
+                </div>
+              ))}
+            </Greybox>
           </div>
           <div className="col-lg-6 col-sm-8 col-12">tiger</div>
           <div className="col-lg-3 d-lg-block d-none">leopard</div>
